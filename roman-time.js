bb9cbda41776975d9de01c20752ee74522fdbfa8
OnaHochetNaMars <onahochetnamars@yandex.ru>
2015-10-06 00:02:32 +0500

    Update roman-time.js

--- roman-time.js
+++ roman-time.js
@@ -1,10 +1,10 @@
 function Rim2(x,Res){
-	if (x>=50) {x-=50; return Rim2(x,Res+'L')}
-	    else if (x>=40) {x-=40; return Rim2(x,Res+'XL')}
-            else if(x>=10) {x-=10; return Rim2(x,Res+'X')}
-			    else if(x==9) {x-=9; return Rim2(x,Res+'IX')}
-                    else if(x>=5) {x-=5; return Rim2(x,Res+'V')}
-                        else if(x>=1) {x-=1; return Rim2(x,Res+'I')}
+    if (x>=50) {return Rim2(x-50,Res+'L')}
+        else if (x>=40) {return Rim2(x-40,Res+'XL')}
+            else if(x>=10) {return Rim2(x-10,Res+'X')}
+	        else if(x==9) {return Rim2(x-9,Res+'IX')}
+                    else if(x>=5) {return Rim2(x-5,Res+'V')}
+                        else if(x>=1) {return Rim2(x-1,Res+'I')}
                             else return Res;
 }
 
