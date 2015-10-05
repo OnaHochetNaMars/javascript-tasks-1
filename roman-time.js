19e5c473c88330e10cbbb45ddce204ecf7f6e4f2
OnaHochetNaMars <onahochetnamars@yandex.ru>
2015-10-06 00:42:58 +0500

    Update roman-time.js

--- roman-time.js
+++ roman-time.js
@@ -1,21 +1,21 @@
 function rim(x,Res){
-if (x>=50) {return rim(x-50,Res+'L')}
-    else if (x>=40) {return rim(x-40,Res+'XL')}
-        else if (x>=10) {return rim(x-10,Res+'X')}
-            else if (x>=9) {return rim(x-9,Res+'IX')}
-                else if (x>=5) {return rim(x-5,Res+'V')}
-                    else if (x>=4) {return rim(x-1,Res+'IV')}
-                        else if (x>=1) {return rim(x-1,Res+'I')}
-                            else return Res;
+    if (x>=50) {return rim(x-50,Res+'L')}
+        else if (x>=40) {return rim(x-40,Res+'XL')}
+            else if (x>=10) {return rim(x-10,Res+'X')}
+                else if (x>=9) {return rim(x-9,Res+'IX')}
+                    else if (x>=5) {return rim(x-5,Res+'V')}
+                        else if (x>=4) {return rim(x-1,Res+'IV')}
+                            else if (x>=1) {return rim(x-1,Res+'I')}
+                                else return Res;
 }
 
-function correctTime(H,M){
-    return (H>=0 && H<=23 && M>=0 && M<=59)
+function correctTime(hours,minutes){
+    return (hours>=0 && hours<=23 && minutes>=0 && minutes<=59)
     }
 
-function romanTime (hour, minute){
-    if (correctTime(hour,minute)) {
-        return rim(hour,'')+' : '+rim(minute,'')
+function romanTime(hours, minutes){
+    if (correctTime(hours,minutes)) {
+        return rim(hours,'')+' : '+rim(minutes,'')
     }
     else return 'Время указано не верно';
 }
